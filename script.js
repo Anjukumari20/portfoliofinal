0c21d27655b212f7cf614355d7a24064656b6fad
Ajay Kumar <ak@Ajays-MacBook-Air.local>
2024-04-09 01:24:34 +0530

    Updated Portflio on 09/04/2024

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Programmer", "Developer", "Data Science Enthusiast", "Computer Science Student", "Engineer"],
+        strings: ["Programmer", "Software Developer", "CS Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Programmer", "Developer", "Data Science Enthusiast", "Computer Science Student", "Engineer"],
+        strings: ["Programmer", "Software Developer", "CS Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
